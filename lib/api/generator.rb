785bbafc963b307119de9a4d430c6cdefc1564ee
shotay79 <sho479@gmail.com>
2023-12-17 10:57:29 +0900

    first commit

---
+++ lib/api/generator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "generator/version"
+
+module Api
+  module Generator
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/api/generator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Api::Generator do
+  it "has a version number" do
+    expect(Api::Generator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
