9c7048a08d409ec8b76e85a661fde90fa1696c9f
shotay79 <sho479@gmail.com>
2023-12-18 11:39:19 +0900

    fix

--- lib/rails/use/railsserializer2types.rb
+++ lib/rails/use/railsserializer2types.rb
@@ -13,9 +13,80 @@ module Rails
         # end
 
         def execute
+          dir = './types'
+          FileUtils.mkdir_p(dir) unless Dir.exist?(dir)
+          serializers_path = Dir.glob('app/serializers/**/*.rb')
+          models = <<~TS
+          TS
+          serializers_path.each do |serializer_path|
+            file_name = File.basename(serializer_path, '.rb')
+            serializer_class = file_name.camelize.constantize
+            interface = serializer_to_interface(serializer_class)
+
+            models += interface
+          end
+
+          File.write(dir + '/models.d.ts', models)
+        end
+
+        private
+
+        def serializer_to_interface(serializer)
+          serializer_attributes = serializer._attributes
+          model_name = serializer.name.gsub('Serializer', '')
+          model_class = model_name.constantize
+          columns = model_class.columns_hash
+          interface_types = serializer_attributes.map do |attribute|
+            column = columns[attribute.to_s]
+            type = if column.nil?
+                     'unknown'
+                   else
+                     _type = type_to_interface_type(column.type)
+                     if column.null
+                       _type += ' | null'
+                     end
+                     _type
+                   end
+            { attribute.to_s.camelize(:lower) => type }
+          end
+          relation_types = OrderSerializer._reflections.map do |association|
+            name = association[0].to_s.camelize(:lower)
+            if association[1].is_a?(ActiveModel::Serializer::BelongsToReflection)
+              next { "#{name}?" => name.camelize }
+            end
+
+            if association[1].is_a?(ActiveModel::Serializer::HasManyReflection)
+              next { "#{name}?" => name.singularize.camelize + '[]' }
+            end
+          end
+          <<~TS
+            export interface #{model_name} {
+              #{
+                interface_types.map do |type|
+                  type.keys[0] + ': ' + type.values[0]
+                end.join("\n\t")
+              }
+              #{
+                relation_types.map do |type|
+                  type.keys[0] + ': ' + type.values[0]
+                end.join("\n\t")
+              }
+            }
+
+          TS
+        end
+
+        def type_to_interface_type(type)
+          case type
+          when :uuid, :datetime, :date, :text
+            'string'
+          when :integer
+            'number'
+          else
+            type.to_s
+          end
         end
       end
     end
   end
 end
-
